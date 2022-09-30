40b4f48783ac15f98a26a702ffbc973cf31350d6
vishal17280 <75197458+vishal17280@users.noreply.github.com>
2022-09-30 15:54:51 +0530

    Add files via upload

---
+++ javascript project racing car/js/main.js
@@ -0,0 +1,45 @@
+var blueCar = document.getElementById("bluecar");
+var raceCar = document.getElementById("racecar");
+var result = document.getElementById("result")
+const score =  document.getElementById("score")
+var game =  document.getElementById("game");
+var counter = 0;
+var jumpsound = document.getElementById("jumpsound")
+
+// move blue car 
+blueCar.addEventListener("animationiteration",function(){
+    var random = ((Math.floor(Math.random()*3))*130);
+     blueCar.style.left = random+"px";
+     counter++
+})
+
+// move racecar
+window.addEventListener("keydown",function(e){
+   if(e.keyCode=="39"){ var raceCarleft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+    if(raceCarleft<260){raceCar.style.left = (raceCarleft + 130) + "px";}
+    jumpsound.play()
+}
+
+
+   if(e.keyCode=="37"){ var raceCarleft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+    if(raceCarleft >0){raceCar.style.left = (raceCarleft - 130) + "px";}
+    jumpsound.play()
+}
+})
+
+
+
+
+//Game over
+setInterval(function Gameover (){
+    var blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"))
+    var blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"));
+    var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+        if((blueCarLeft === raceCarLeft) && (blueCarTop > 250) && (blueCarTop < 450)){
+            result.style.display = "block";
+            game.style.display = "none";
+            score.innerHTML = `score: ${counter} `;
+
+            counter = 0;
+        }
+}, 10)
